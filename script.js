8f7c7ead3d3a61bf5d1158e8849cbc393a39bdee
DanielGar12 <garcia.daniel23256@gmail.com>
2025-03-05 11:00:31 -0600

    stuff

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ const initMap = () => {
     })
 
 }
+
+
+document.getElementById("demo").addEventListener("click", function(){
+    
+    this.style.color = (this.style.color === "red") ? "white" : "red";
+});
+
+document.getElementById("dare").addEventListener("click", function(){
+    console.log("hello")
+    this.style.color = (this.style.color === "white") ? "red" : "white"; 
+});
+
+console.log(getElementById("dare"))
